bd9d3da855eadc4130c01d03fa8a402dffc44cbd
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-12 14:12:27 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -11,6 +11,12 @@ class Timer extends Component {
   }
 
   //Your code here
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 
   componentDidMount() {
     this.interval = setInterval(
